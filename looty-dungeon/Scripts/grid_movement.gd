459cb9235d846b5da0fb835830730864056534ab
hipo32 <visa.alex11@gmail.com>
2026-05-01 19:18:16 +0200

    Added functionality from player to grid movement

--- looty-dungeon/Scripts/grid_movement.gd
+++ looty-dungeon/Scripts/grid_movement.gd
@@ -4,17 +4,61 @@ extends Node
 @export var grid_size:float = 1
 @export var step_time:float = 0.5
 
+@export var body:Node3D
+@export var rotate_speed:float
+
+@export var ground_raycast:RayCast3D
+var on_ground = false
+
+@export_flags_3d_physics var enemy_collision
+
 var is_moving = false
 signal finished_step()
 
+signal on_wall
+signal on_entity
+
 var parent:Node3D
+
+var rot_tween: Tween
 
 func _ready() -> void:
 	parent = get_parent() as Node3D
 
+func _process(delta: float) -> void:
+	on_ground = ground_raycast.is_colliding()
+
 func move(direction:global.direction):
 	if is_moving: return
 	var dir = global.dir_to_vec(direction)
+	
+	#rotate body
+	var new_angle = (dir*Vector2(1,-1)).angle() + PI/2
+	var angle_dif = angle_difference(body.rotation.y,new_angle)
+	var new_rotation = Vector3(0,angle_dif,0)
+	if rot_tween: rot_tween.kill()
+	rot_tween = create_tween()
+	rot_tween.set_ease(Tween.EASE_IN_OUT)
+	rot_tween.tween_property(body,"rotation",new_rotation,abs(angle_dif)/rotate_speed).as_relative()
+	
+	#check walls or enemies
+	var dir_3d = Vector3(dir.x,0,dir.y)
+	var space = parent.get_world_3d().direct_space_state
+	
+	var wall_query = PhysicsRayQueryParameters3D.create(parent.position,parent.position+dir_3d,1)
+	var result = space.intersect_ray(wall_query)
+	if result:
+		on_wall.emit()
+		return
+	
+	var enemy_query = PhysicsRayQueryParameters3D.create(parent.position,parent.position+dir_3d,enemy_collision) #TODO: review masks
+	enemy_query.collide_with_areas = true #Detectar hitboxes
+	enemy_query.collide_with_bodies = false
+	result = space.intersect_ray(enemy_query)
+	if result:
+		on_entity.emit()
+		return
+	
 	var new_pos = parent.position + Vector3(grid_size*dir.x,0,grid_size*dir.y)
 	
 	is_moving = true
--- looty-dungeon/Scripts/player.gd
+++ looty-dungeon/Scripts/player.gd
@@ -3,28 +3,22 @@ extends Node3D
 @onready var grid_movement: GridMovement = $GridMovement
 @onready var step_timer: Timer = $Step_timer
 @onready var body: Node3D = $Body
-@onready var ground_check: RayCast3D = $GroundCheck
 @onready var health: Health = $Health
 @onready var invincibility_timer: Timer = $"Invincibility timer"
 @onready var animator: AnimationPlayer = $AnimationPlayer
 @onready var attack_timer: Timer = $"Attack timer"
 
-@export var rotate_speed:float
-
-@export_flags_3d_physics var enemy_collision
-
 @export var Attack_animation:String
 @export var Action_animation:String
 
 var can_move:bool = true
-var on_ground:bool = true
 
 var just_attacked:bool = false
 
-var tween: Tween
-
 func _ready() -> void:	
 	invincibility_timer.timeout.connect(end_invincibility)
+	
+	grid_movement.on_entity.connect(attack)
 	
 	health.got_hit.connect(on_hit)
 	health.death.connect(die)
@@ -32,7 +26,11 @@ func _ready() -> void:
 	animator.play("Basic/Idle")
 
 func _process(delta: float) -> void:
-	on_ground = ground_check.is_colliding()
+	print(grid_movement.on_ground)
+	if grid_movement.is_moving:
+		animator.play("Basic/Walk")
+	elif can_move:
+		animator.play("Basic/Idle")
 
 func _physics_process(_delta: float) -> void:
 	if can_move:
@@ -40,46 +38,18 @@ func _physics_process(_delta: float) -> void:
 		if grid_movement.is_moving or step_timer.time_left > 0: return
 		var dir = Vector2(Input.get_axis("move_right","move_left"),Input.get_axis("move_down","move_up"))
 		if dir.length() <= 0: 
-			animator.play("Basic/Idle")
+			#animator.play("Basic/Idle")
 			just_attacked = false
 			return
 		var direction = global.vec_to_dir(dir)
-		dir = global.dir_to_vec(direction) #No diagonales
-		
-		#rotate body
-		var new_angle = (dir*Vector2(1,-1)).angle() + PI/2
-		var angle_dif = angle_difference(body.rotation.y,new_angle)
-		var new_rotation = Vector3(0,angle_dif,0)
-		if tween: tween.kill()
-		tween = create_tween()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(body,"rotation",new_rotation,abs(angle_dif)/rotate_speed).as_relative()
-		
-		#check walls or enemies
-		var dir_3d = Vector3(dir.x,0,dir.y)
-		var space = get_world_3d().direct_space_state
-		
-		var wall_query = PhysicsRayQueryParameters3D.create(position,position+dir_3d,1)
-		var result = space.intersect_ray(wall_query)
-		if result:
-			#TODO: Wall animation
-			return
-		
-		var enemy_query = PhysicsRayQueryParameters3D.create(position,position+dir_3d,enemy_collision) #TODO: review masks
-		enemy_query.collide_with_areas = true #Detectar hitboxes
-		enemy_query.collide_with_bodies = false
-		result = space.intersect_ray(enemy_query)
-		if result:
-			if not just_attacked: attack()
-			return
 		
 		#move
-		animator.play("Basic/Walk")
 		grid_movement.move(direction)
-		await grid_movement.finished_step
+		await  grid_movement.finished_step
 		step_timer.start()
 
 func attack():
+	if just_attacked: return
 	just_attacked = true
 	can_move = false
 	animator.play(Attack_animation)
