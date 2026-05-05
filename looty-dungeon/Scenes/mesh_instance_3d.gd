e53a7722fa61af4365a312b0a1ee32201c096486
Wen <wenqiang.he@estudiantat.upc.edu>
2026-05-05 11:17:39 +0200

    Axe_Trap

---
+++ looty-dungeon/Scenes/mesh_instance_3d.gd
@@ -0,0 +1,18 @@
+extends MeshInstance3D
+
+func _ready():
+	rotar_loop()
+
+func rotar_loop():
+	var tween = create_tween()
+	
+	tween.tween_property(
+		self,
+		"rotation_degrees:y",
+		rotation_degrees.y + 90,
+		1.0
+	)
+	
+	tween.tween_interval(1.0) 
+	
+	tween.finished.connect(rotar_loop)
