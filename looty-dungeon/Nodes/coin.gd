249532cf9a75da7aaa531074db9a7b1c0dc0d135
wen <wenqianghe10@gmail.com>
2026-05-25 23:05:36 +0200

    Coins

---
+++ looty-dungeon/Nodes/coin.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+const ROTATE = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(deg_to_rad(ROTATE))
+	
+
+
+	
+
+
+func _on_body_entered(body: Node3D) -> void:
+	queue_free()
